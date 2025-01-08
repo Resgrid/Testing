0c388634d38e8fabbc674a93f3691f02427e6a01
Shawn Jackson <shawn@resgrid.com>
2025-01-08 08:29:13 -0800

    added tc reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* reporter: 'html',*/
+  reporter: [
+    ['playwright-teamcity-reporter', {'testMetadataArtifacts': 'test-results', logConfig: false}],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
